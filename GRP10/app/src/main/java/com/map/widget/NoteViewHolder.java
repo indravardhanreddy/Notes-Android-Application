ef6c7c7449c7adc61869ddde50bb4233ba553fb2
indravardhanreddym <moramindravardhanreddy@gmail.com>
2021-04-05 23:31:11 +0530

    New

---
+++ GRP10/app/src/main/java/com/map/widget/NoteViewHolder.java
@@ -0,0 +1,37 @@
+package com.map.widget;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+import com.map.R;
+import com.map.inner.Formatter;
+import com.map.model.DatabaseModel;
+import com.map.model.Note;
+import com.map.widget.template.ModelViewHolder;
+
+public class NoteViewHolder extends ModelViewHolder<Note> {
+	public ImageView badge;
+	public TextView title;
+	public TextView date;
+
+	public NoteViewHolder(View itemView) {
+		super(itemView);
+		badge = (ImageView) itemView.findViewById(R.id.badge_icon);
+		title = (TextView) itemView.findViewById(R.id.title_txt);
+		date = (TextView) itemView.findViewById(R.id.date_txt);
+	}
+
+	@Override
+	public void populate(Note item) {
+		if (item.type == DatabaseModel.TYPE_NOTE_DRAWING) {
+			badge.setImageResource(R.drawable.fab_drawing);
+		} else {
+			badge.setImageResource(R.drawable.fab_type);
+		}
+		title.setText(item.title);
+		date.setText(Formatter.formatShortDate(item.createdAt));
+	}
+}
